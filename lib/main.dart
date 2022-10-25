e5682fd7de2ca168691405cc671b07fd39947721
Refay <mohamedrefaydeen2000@gmail.com>
2022-10-25 18:26:08 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,86 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Miit',
       theme: ThemeData.dark().copyWith(
-          //default color
-          scaffoldBackgroundColor: kPrimaryColor,
-          primaryColor: kPrimaryColor,
-          textTheme: GoogleFonts.sourceSansProTextTheme(
-            Theme.of(context).textTheme.apply().copyWith(
-                bodyText1: TextStyle(
+        //default color
+        scaffoldBackgroundColor: kPrimaryColor,
+        primaryColor: kPrimaryColor,
+        textTheme:
+            GoogleFonts.sourceSansProTextTheme(Theme.of(context).textTheme)
+                .apply()
+                .copyWith(
+                    bodyText1: TextStyle(
+                        color: kTextWhiteColor,
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold),
+              bodyText2: TextStyle(
+                  color: kTextWhiteColor,
+                  fontSize: 28.0,
+                ),
+
+              subtitle1: TextStyle(
                     color: kTextWhiteColor,
-                    fontSize: 35.0,
-                    fontWeight: FontWeight.bold),
-               subtitle2: TextStyle(color:kTextWhiteColor,fontSize: 18.0,
-               fontWeight: FontWeight.w300)),
-          )),
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.w500),
+                    subtitle2: TextStyle(
+                        color: kTextWhiteColor,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w300),),
+        //inputdecoration theme for all our the app
+        inputDecorationTheme: InputDecorationTheme(
+          //label style for formField
+          labelStyle: TextStyle(
+            fontSize: 18.0,
+            color: kTextLightColor,
+            height: 0.5,
+          ),
+          //hint style
+          hintStyle: TextStyle(
+            fontSize: 16.0,
+            color: kTextBlackColor,
+            height: 0.5,
+          ),
+          //we aree uusing underline input border
+          //not outline
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: kTextLightColor,
+              width: 0.7,
+            ),
+          ),
+          border: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: kTextLightColor,
+            ),
+          ),
+          disabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: kTextLightColor,
+            ),
+          ),
+          //on focus change color
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: kPrimaryColor,
+              //width:0.7,
+            ),
+          ),
+          //color changes when user enters wrong information
+          //we will use validation for this process
+          errorBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: kErrorBorderColor,
+              width: 1.2,
+            ),
+          ),
+          //same on focus error color
+          focusedErrorBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: kErrorBorderColor,
+              width: 1.2,
+            ),
+          ),
+        ),
+      ),
       //initial
       initialRoute: SplashScreen.routeName,
       routes: routes,
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,13 +1,13 @@
+import 'package:college_app/screens/home_screen/home_screen.dart';
 import 'package:college_app/screens/login_screen/login_screen.dart';
 import 'package:college_app/screens/splashscreen/splash_screen.dart';
-import 'package:college_app/screens/welcome_page/welcome_page.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-Map<String,WidgetBuilder> routes= {
-
+Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
   //WelcomePage.routeName: (context) => WelcomePage(),
-  LoginScreen.routeName: (context) => LoginScreen()
-
- };
+  LoginScreen.routeName: (context) => LoginScreen(),
+  HomeScreen.routeName: (context) => HomeScreen(),
+};
